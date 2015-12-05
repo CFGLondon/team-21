2833e0555ced50e25920c273b1cb7a039096dd89
Radu <r.blana@warwick.ac.uk>
2015-12-05 10:11:52 +0000

    completed dashboard thingy

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -25,108 +25,19 @@ angular.module('myApp.controllers', []).
     // write Ctrl here
   }).
 controller('bangladeshCtrl', function ($scope, $http) {
-        $(function ($scope) {
-
-            //Load Data
-            $http.get('/api/bangladesh').
-                success(function(data, status, headers, config) {
-                    $scope.dataSet = data.data;
-                    console.log($scope.dataSet);
-
-                    $.getJSON('/assets/maps/bd-all.json', function (geojson) {
-                        $('#container').highcharts('Map', {
-
-                            title : {
-                                text : 'People with  Impairments in Bangladesh'
-                            },
-                            chart: {
-                               backgroundColor: 'rgba(255,255,255,0.0)',
-                            },
-
-                            mapNavigation: {
-                                enabled: true,
-                                buttonOptions: {
-                                    verticalAlign: 'bottom'
-                                }
-                            },
-
-                            colorAxis: {
-                                minColor: '#FFFFFF',
-                                maxColor: '#FFA500'
-                            },
-                            series : [
-
-                                {
-                                data :  $scope.dataSet,
-                                mapData: geojson,
-                                allowPointSelect:true,
-                                joinBy: 'name',
-                                name: 'No of impaired people',
-                                states: {
-                                    hover: {
-                                        color: '#EEE8AA'
-                                    }
-                                },
-                                dataLabels: {
-                                    enabled: false,
-                                    format: '{point.properties.name}'
-                                }
-                            }]
-                        });
-                    });
-                });
-            // Prepare random data
-            var data = [];
-            $(document).ready(function () {
-
-                // Build the chart
-                $('#pie').highcharts({
-                    chart: {
-                        plotBackgroundColor: null,
-                        plotBorderWidth: null,
-                        plotShadow: false,
-                        type: 'pie',
-                         backgroundColor: 'rgba(255,255,255,0.0)',
-                    },
-                    title: {
-                        text: 'Impairment Types'
-                    },
-                    tooltip: {
-                        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-                    },
-                    plotOptions: {
-                        pie: {
-                            allowPointSelect: true,
-                            cursor: 'pointer',
-                            dataLabels: {
-                                enabled: false
-                            },
-                            showInLegend: true
-                        }
-                    },
-                    series: [{
-                        name: 'Brands',
-                        colorByPoint: true,
-                        data: [{
-                            name: 'Physical',
-                            y: 56.33
-                        }, {
-                            name: 'Sensory',
-                            y: 24.03
-                        }, {
-                            name: 'Speech and Language',
-                            y: 10.38
-                        }, {
-                            name: 'Learning',
-                            y: 4.77
-                        }, {
-                            name: 'Cognitive',
-                            y: 0.91
-                        }]
-                    }]
-                });
-            });
-        })
+  var placeholderDiv = document.getElementById("tableauViz");
+  var url = "https://public.tableau.com/shared/53F327G7T?:display_count=yes";
+  var options = {
+    width: placeholderDiv.offsetWidth,
+    height: placeholderDiv.offsetHeight,
+    hideTabs: true,
+    hideToolbar: true,
+    onFirstInteractive: function () {
+      workbook = viz.getWorkbook();
+      activeSheet = workbook.getActiveSheet();
+    }
+    };
+    var viz = new tableau.Viz(placeholderDiv, url, options);
     }).
     controller('interactiveMapCtrl', function ($scope) {
     }).
